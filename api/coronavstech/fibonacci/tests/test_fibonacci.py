c2a963f920359ad2512e14e0de19f5fbc318e191
PetroLakhai <pete.lakhay@gmail.com>
2022-10-30 22:31:00 +0200

    fibonacci tests

--- api/coronavstech/fibonacci/tests/test_fibonacci.py
+++ api/coronavstech/fibonacci/tests/test_fibonacci.py
@@ -1,10 +1,13 @@
+from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
-from my_decorator import my_parametrized
+from typing import Callable
+import pytest
+# from my_decorator import my_parametrized
 
 
-# @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-@my_parametrized(
-    identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n: int, expected: int) -> None:
-    res = fibonacci_naive(n=n)
+# @my_parametrized(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
+@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+    res = fib_func(n)
     assert res == expected
