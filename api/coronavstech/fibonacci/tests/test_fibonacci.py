f19f9680f7d4baa9ce49700a4901f9f121369686
PetroLakhai <pete.lakhay@gmail.com>
2022-11-02 20:51:53 +0200

    application of time tracker

--- api/coronavstech/fibonacci/tests/test_fibonacci.py
+++ api/coronavstech/fibonacci/tests/test_fibonacci.py
@@ -1,13 +1,19 @@
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 from typing import Callable
+from fixtures import time_tracker
 import pytest
+
 # from my_decorator import my_parametrized
 
 
 # @my_parametrized(identifiers="n,expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
-@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize(
+    "fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
+)
 @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+def test_fibonacci(
+    time_tracker, fib_func: Callable[[int], int], n: int, expected: int
+) -> None:
     res = fib_func(n)
     assert res == expected
